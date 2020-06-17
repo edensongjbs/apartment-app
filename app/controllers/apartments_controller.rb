babdc35bb2b048a10659b51bd3fdfc8c845754ad
edensongjbs <edensongjbs@gmail.com>
2020-06-17 17:01:43 -0400

    working version

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,3 +1,54 @@
 class ApartmentsController < ApplicationController
   # add controller methods
+  set :views, 'app/views/apartments'
+
+  get '/apartments' do
+    @apartments = Apartment.all
+    erb :index
+  end
+
+  get '/apartments/new' do
+    @tenants = Tenant.all
+    erb :new
+  end
+  
+  get '/apartments/:id' do
+    @apartment = Apartment.find(params[:id])
+    erb :show
+  end
+
+  post '/apartments' do
+    apartment= Apartment.create(params[:apartment])
+    # binding.pry
+    # unless params[:apartment][:address].empty?
+    #   new_apartment = Apartment.create(params[:apartment])
+    #   Apartment.apartment = new_apartment
+    # end
+    # Apartment.save
+    redirect "/apartments/#{apartment.id}"
+  end
+
+  patch '/apartments/:id' do
+    apartment = Apartment.find(params[:id])
+    apartment.update(params[:apartment])
+    # unless params[:apartment][:address].empty?
+    #   new_apartment = Apartment.create(params[:apartment])
+    #   Apartment.update(apartment: new_apartment)
+    # end
+    redirect "/apartments/#{apartment.id}"
+  end
+
+  get '/apartments/:id/edit' do
+    @apartment = Apartment.find(params[:id])
+    # @apartments = Apartment.all
+    #binding.pry
+    erb :edit
+  end
+  
+  delete '/apartments/:id' do
+    apartment = Apartment.find(params[:id])
+    apartment.destroy
+    redirect "/apartments"
+  end
+
 end
--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,3 +1,54 @@
 class TenantsController < ApplicationController
   # add controller methods
+  set :views, 'app/views/tenants'
+
+  get '/tenants' do
+    @tenants = Tenant.all
+    erb :index
+  end
+
+  get '/tenants/new' do
+    @apartments = Apartment.all
+    erb :new
+  end
+  
+  get '/tenants/:id' do
+    @tenant = Tenant.find(params[:id])
+    erb :show
+  end
+
+  post '/tenants' do
+    tenant= Tenant.new(params[:tenant])
+    unless params[:apartment][:address].empty?
+      new_apartment = Apartment.create(params[:apartment])
+      tenant.apartment = new_apartment
+    end
+    tenant.save
+    redirect "/tenants/#{tenant.id}"
+  end
+
+  patch '/tenants/:id' do
+    tenant = Tenant.find(params[:id])
+    tenant.update(params[:tenant])
+    unless params[:apartment][:address].empty?
+      new_apartment = Apartment.create(params[:apartment])
+      tenant.update(apartment: new_apartment)
+    end
+    redirect "/tenants/#{tenant.id}"
+  end
+
+  get '/tenants/:id/edit' do
+    @tenant = Tenant.find(params[:id])
+    @apartments = Apartment.all
+    #binding.pry
+    erb :edit
+  end
+
+  delete '/tenants/:id' do
+    tenant = Tenant.find(params[:id])
+    tenant.destroy
+    redirect "/tenants"
+  end
+
+
 end
